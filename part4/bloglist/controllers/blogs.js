369f198b8cc40c0bc079e2b3df3e2c2b2e78c307
unknown <zh901008@gmail.com>
2020-03-28 22:41:30 -0400

    add test api for End to end testing

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -26,7 +26,7 @@ blogsRouter.post('/', async (request, response, next) => {
       author: body.author,
       url: body.url,
       likes: body.likes === undefined ? 0 : body.likes,
-      user: user._id
+      user: user
     });
 
     const savedBlog = await blog.save();
