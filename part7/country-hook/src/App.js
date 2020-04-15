f2d5288de5917a215fa1425247cd8992a398385f
unknown <zh901008@gmail.com>
2020-04-15 19:31:52 -0400

    fixed to caught the error

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -18,11 +18,11 @@ const useField = type => {
 const useCountry = name => {
   const [country, setCountry] = useState(null);
   useEffect(() => {
-    if (name !== '') {
+    if (name) {
       axios
         .get(`https://restcountries.eu/rest/v2/name/${name}?fullText=true`)
         .then(response => setCountry({ data: response.data[0], found: true }))
-        .catch(setCountry({ found: false }));
+        .catch(error =>setCountry({ found: false }));
     }
   }, [name]);
   return country;
