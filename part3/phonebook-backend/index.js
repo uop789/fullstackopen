291bff8a4ed1a136f486fed73efaeb418923b847
unknown <zh901008@gmail.com>
2021-09-13 10:49:16 -0500

    add get post delete htttp request for testing and add the morgan middleware to application for logging the request infos

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,141 +1,98 @@
-/* eslint-disable no-unused-vars */
-require('dotenv').config();
 const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
 const morgan = require('morgan');
-const Person = require('./models/person');
 
-app.use(bodyParser.json());
+const app = express();
 
-const cors = require('cors');
-app.use(cors());
-
-morgan.token('data', function(req, res) {
-  if (req.method === 'POST') {
-    return `{"name":"${req.body.name}","number":"${req.body.number}"}`;
-  } else {
-    return null;
-  }
+morgan.token('body', function (req) {
+	return JSON.stringify(req.body);
 });
+
+app.use(express.json());
 app.use(
-  morgan(':method :url :status :res[content-length] - :response-time ms :data')
+	morgan(':method :url :status :res[content-length] - :response-time ms :body')
 );
 
-app.use(express.static('build'));
+let persons = [
+	{
+		id: 1,
+		name: 'Arto Hellas',
+		number: '040-123456',
+	},
+	{
+		id: 2,
+		name: 'Ada Lovelace',
+		number: '39-44-5323523',
+	},
+	{
+		id: 3,
+		name: 'Dan Abramov',
+		number: '12-43-234345',
+	},
+	{
+		id: 4,
+		name: 'Mary Poppendieck',
+		number: '39-23-6423122',
+	},
+];
 
-app.get('/api/persons', (req, res) => {
-  Person.find({}).then(persons => {
-    res.json(persons.map(person => person.toJSON()));
-  });
-});
-
-app.get('/info', (req, res, next) => {
-  Person.countDocuments({})
-    .then(result => {
-      res.send(`<p>Phonebook has info for ${result} people</p>
-              <p>${new Date()}</p>    
-    `);
-    })
-    .catch(error => next(error));
-});
-
-app.get('/api/persons/:id', (req, res, next) => {
-  Person.findById(req.params.id)
-    .then(person => {
-      if (person) {
-        res.json(person.toJSON());
-      } else {
-        res.status(404).end();
-      }
-    })
-    .catch(error => {
-      next(error);
-    });
-});
-
-app.delete('/api/persons/:id', (req, res, next) => {
-  Person.findByIdAndRemove(req.params.id)
-    .then(result => {
-      res.status(204).end();
-    })
-    .catch(error => next(error));
-});
-
-app.post('/api/persons', (req, res, next) => {
-  const body = req.body;
-  // if (!body.name || !body.number) {
-  //   return res.status(400).json({
-  //     error: 'name or number missing'
-  //   });
-  // }
-
-  // Person.find({}).then(persons => {
-  //   const nameArr = persons.map(person => person.name);
-  //   if (nameArr.indexOf(body.name) !== -1) {
-  //     return res.status(400).json({
-  //       error: 'name must be unique'
-  //     });
-  //   } else {
-  const person = new Person({
-    name: body.name,
-    number: body.number
-  });
-  person
-    .save()
-    .then(savedPerson => savedPerson.toJSON())
-    .then(savedAndFormattedPerson => {
-      res.json(savedAndFormattedPerson);
-    })
-    .catch(error => next(error));
-});
-
-app.put('/api/persons/:id', (req, res, next) => {
-  const body = req.body;
-  const person = {
-    name: body.name,
-    number: body.number
-  };
-  if (person.number.length >= 8) {
-    Person.countDocuments({ name: person.name }).then(result => {
-      if (result === 0) {
-        res.status(400).send({
-          error: `Information of ${person.name} has already been removed from server`
-        });
-      } else {
-        Person.findByIdAndUpdate(req.params.id, person, { new: true })
-          .then(updatedPerson => res.json(updatedPerson.toJSON()))
-          .catch(error => next(error));
-      }
-    });
-  } else {
-    res
-      .status(400)
-      .send({ error: 'Minimum length of number is 8, please change it!' });
-  }
-});
-
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' });
+const generateId = () => {
+	return Math.floor(Math.random() * 10000);
 };
 
-app.use(unknownEndpoint);
+app.get('/info', (request, response) => {
+	const content = `
+		Phonebook has info for ${persons.length} people
+		<br/><br/>
+		${new Date()}
+	`;
+	response.send(content);
+});
 
-const errorHandler = (error, req, res, next) => {
-  console.log(error.message);
+app.get('/api/persons', (request, response) => {
+	response.json(persons);
+});
 
-  if (error.name === 'CastError' && error.kind === 'ObjectId') {
-    return res.status(400).send({ error: 'malformatted id' });
-  } else if (error.name === 'ValidationError') {
-    return res.status(400).json({ error: error.message });
-  }
+app.get('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id);
+	const toFind = persons.find((p) => p.id === id);
+	if (!toFind) {
+		return response.status(404).end();
+	}
+	response.json(toFind);
+});
 
-  next(error);
-};
+app.post('/api/persons', (request, response) => {
+	const body = request.body;
+	if (!body.name || !body.number) {
+		return response.status(400).json({
+			error: 'name or number missing',
+		});
+	}
 
-app.use(errorHandler);
+	if (persons.find((p) => p.name === body.name)) {
+		return response.status(400).json({
+			error: 'name must be unique',
+		});
+	}
 
-const PORT = process.env.PORT;
+	const person = {
+		id: generateId(),
+		name: body.name,
+		number: body.number,
+	};
+
+	persons = persons.concat(person);
+
+	response.json(person);
+});
+
+app.delete('/api/persons/:id', (request, response) => {
+	const id = Number(request.params.id);
+	persons = persons.filter((p) => p.id !== id);
+	response.status(204).end();
+});
+
+const PORT = 3001;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+	console.log(`Server running on port ${PORT}`);
 });
